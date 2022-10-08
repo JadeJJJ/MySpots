06ec10aa0f96abc566c7f7dde8420e8b240e7c89
JayDWrld <101358812+JayDWrld@users.noreply.github.com>
2022-10-08 12:45:30 +0200

    Register Page Code

--- app/src/main/java/com/example/myspots/MainActivity.java
+++ app/src/main/java/com/example/myspots/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.myspots;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
+
 }
