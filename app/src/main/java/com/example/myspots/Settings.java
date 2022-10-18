962214c7d6e02cb1c96137d3fbd3329d98ee54a2
Dylan Ainsworth <dylan.ainsworth@gmail.com>
2022-10-18 11:59:50 +0200

    navbar still busy

--- app/src/main/java/com/example/myspots/Settings.java
+++ app/src/main/java/com/example/myspots/Settings.java
@@ -4,11 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Settings extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+//navBar
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+
+
+
+public class Settings extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    //navBar
+    private DrawerLayout mDrawerLayout; //DylanA
+    private ActionBarDrawerToggle mToggle; //DylanA
+    private NavigationView navView; //DylanA
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {//DylanA
+
+        if(mToggle.onOptionsItemSelected(item)){//DylanA
+            return true;//DylanA
+        }
+
+        return super.onOptionsItemSelected(item);//DylanA
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_HomePage:
+                // startActivity(new Intent(Settings.this, HomePage.class));
+                break;
+            case R.id.nav_LandmarkListPage:
+
+                // startActivity(new Intent(HomePage.this, LandmarkListPage.class));
+                break;
+
+        }
+        return true;
     }
 }
